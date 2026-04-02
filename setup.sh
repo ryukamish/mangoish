5dd727e16a454bba040288b63d567a0c9d7d335f
ryukamish <asuskr25@gmail.com>
2026-04-02 23:00:23 +0530

    add: bash script to setup mangowm

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 # install packages
-sudo pacman -S swww rofi hypridle wlopm mango alacritty waybar ddcutil xdg-desktop-portal-gtk xdg-desktop-portal-wlr nautilus grim slurp wlsunset librewolf-bin ungoogled-chromium-bin imagemagick stow
+sudo pacman -S swww rofi hypridle wlopm mango alacritty waybar ddcutil xdg-desktop-portal-gtk xdg-desktop-portal-wlr nautilus grim slurp wlsunset imagemagick stow
+yay -S librewolf-bin ungoogled-chromium-bin
+
+# ddcutil
+sudo modprobe i2c-dev
+sudo usermod -aG i2c $USER
+sudo chmod a+rw /dev/i2c-*
