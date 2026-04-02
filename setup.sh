7b4363eb98c23472fc25a374dc2113d7e1718419
ryukamish <asuskr25@gmail.com>
2026-04-02 17:39:07 +0530

    intial commit

---
+++ setup.sh
@@ -0,0 +1,2 @@
+# install packages
+sudo pacman -S swww rofi hypridle wlopm mango alacritty waybar ddcutil xdg-desktop-portal-gtk xdg-desktop-portal-wlr nautilus grim slurp wlsunset librewolf-bin ungoogled-chromium-bin imagemagick stow
